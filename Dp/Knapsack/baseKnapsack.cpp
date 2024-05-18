5730fda54eed3fa683fdd209d47e4519b918c106
sonuarman <mansooriarman090@gmail.com>
2024-05-19 01:44:34 +0530

    catalan

--- Dp/Knapsack/baseKnapsack.cpp
+++ Dp/Knapsack/baseKnapsack.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 //recursion code for knapsack
+int knapsack(vector<int> weight,vector<int> value  ){
+    int n= 
+    int m=
+    int value
+    //knapsack me le sakte hai ki nahi ye chekc karna hai 
+    if(/*le sakte hai*/wt[n-1]<c ){
+      // yaha pe kuch karna hoga 
+      knapsack(value+)
+    }
+    //agr ni le sakte hai to 
+    return max
+}
 
 int main(){
     vector<int> weight{};
