52163367244be9b1f599cd4da03f5cbcf7ee61d9
sonuarman <mansooriarman090@gmail.com>
2024-05-19 05:34:10 +0530

    bottom up

--- Dp/CatalanBased/catalan.cpp
+++ Dp/CatalanBased/catalan.cpp
@@ -1,3 +1,4 @@
+//memoised version
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -46,3 +47,14 @@ int main() {
 
     return 0;
 }
+
+/* bottom up approach 
+vector<int>dp(n+1,0);
+	dp[0]=dp[1]=1;
+	for(int i=2;i<=n;i++){ //here take care of the case <=n
+	    for(int j=0;j<i;j++){
+	        dp[i]+=dp[j]*dp[i-j-1];
+	    }
+	}
+	cout<<dp[n];
+*/
